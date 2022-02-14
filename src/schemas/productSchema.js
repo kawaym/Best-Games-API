c5a1bae333e6a75eba9a10e8d0e4600ce7b69734
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2022-02-14 15:06:40 -0300

    fix: product schema
    Signed-off-by: Kaway Marinho <kawayrmarinho@poli.ufrj.br>

--- src/schemas/productSchema.js
+++ src/schemas/productSchema.js
@@ -4,7 +4,8 @@ const productSchema = joi.object({
   name: joi.string().max(15).required(),
   price: joi.number().required(),
   description: joi.string().required(),
-  image: joi.string().uri().required()
+  image: joi.string().uri().required(),
+  category: joi.string.valid('games', 'consoles', 'collectibles', 'accessories')
 });
 
 export default productSchema;
