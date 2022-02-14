f61e871d84b984a68b780fe1a8c2f2b6199ca2c8
Kaway Marinho <kawayrmarinho@poli.ufrj.br>
2022-02-14 14:46:15 -0300

    fix: cors policy

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const server = express();
-server.use(cors());
+server.use(cors({origin: /\.herokuapp\.com$/}));
 server.use(json());
 server.use(router);
 
